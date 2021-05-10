9f6dffec099d9f6810ad9c6b0623b2e7e2dc23d0
fizashalam <fizashalam.ai20@bmsce.ac.in>
2021-05-10 15:45:29 +0530

    adding program 1 and its snapshot

---
+++ program1.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main() {
+
+ int a, b ; // inputing two numbers a and b
+ printf("input number 1:");
+ scanf("%d",&a);
+ printf("input number 2:");
+ scanf("%d",&b);
+ int sum= a + b;
+ printf("sum of number 1 and number 2=%d", sum);
+ return 0;
+}
