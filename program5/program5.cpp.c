85b88f976131e487cb47da03fb2f9dc07e37a176
fizashalam <fizashalam.ai20@bmsce.ac.in>
2021-05-17 09:58:41 +0530

    changing file extension and adding snapshot

---
+++ program5/program5.cpp.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+float a,b;
+float product=0.0;
+print("enter the value of a");
+scanf("%f",&a);
+printf("enter the value of b");
+scanf("%f",&b);
+product=a*b;
+printf("%f",product);
+return 0;
+}
